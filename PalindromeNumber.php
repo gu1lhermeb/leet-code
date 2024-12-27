94da3aa1888072fbd647e392307d55fbb769bbea
gu1lhermeb <guilhermebraz089@gmail.com>
2024-12-26 22:44:40 -0300

    feat: cria PalindromeNumber

---
+++ PalindromeNumber.php
@@ -0,0 +1,22 @@
+<?php
+
+class Solution {
+
+    function isPalindrome($number) {
+        $arrayNumber = str_split($number);
+
+        $reverseArrayNumber = array_reverse($arrayNumber);
+
+        if ($arrayNumber === $reverseArrayNumber) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+$number = -121;
+
+$palindrome = new Solution();
+
+$palindrome->isPalindrome($number);
